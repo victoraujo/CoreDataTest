5fc659aeb9bc38a1e98f2a46e3edf18e83787078
Victor Vieira <avieira.victor@gmail.com>
2020-08-19 14:54:10 -0300

    Initial Commit

---
+++ CoreDataTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by Victor Vieira on 19/08/20.
+//  Copyright © 2020 Victor Vieira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
